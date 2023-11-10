e9b67f8db7a96f9ca822ec34fed6454905592947
Shepamat000 <shepamat000@gmail.com>
2023-11-10 02:50:20 -0600

    Update Level.java

--- MouseFan/Level.java
+++ MouseFan/Level.java
@@ -1,9 +1,12 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 
 /** Fan Engine by Matthew Shepard
  *  November 2023
  */
-import java.awt.Graphics;
-import java.util.ArrayList;
 
 public class Level {
 	
@@ -23,6 +26,24 @@ public class Level {
 }
 
 	class Wall {
+		
+		int x;
+		int y; 
+		int width;
+		int length;
+		double rotation;
+		
+		public void paint(Graphics g) {
+			// Test Rectangle
+			g.setColor(Color.gray);
+			Graphics2D g2d = (Graphics2D)g;
+			Rectangle rect = new Rectangle (260, 260, 480, 480);
+			g2d.rotate(Math.toRadians(rotation), 240+480/2, 240+480/2);
+			g2d.fill(rect);
+			g2d.rotate(Math.toRadians(-rotation), 240+480/2, 240+480/2);
+			rotation+= .1;
+		}
+
 		 
 	}
 
